ee5b9d0065da2c0540a7c28c12baff08645c4e91
Luche Pousa <luchepousa@MacBook-Pro-de-Luche.local>
2025-05-27 18:06:58 -0300

    Primer entrega proyecto TechLab

---
+++ index.js
@@ -0,0 +1,69 @@
+const URL = 'https://fakestoreapi.com/products';
+
+let [,, metodo, recurso, ...extras] = process.argv;
+
+if (recurso.includes('/')) {
+    const [parte1, parte2] = recurso.split('/');
+    recurso = parte1;
+    extras = [parte2, ...extras];
+}
+
+const fetchData = async () => {
+    try {
+        if (metodo === 'GET' && recurso === 'products') {
+            if (extras.length === 0) {
+                const response = await fetch(URL);
+                const data = await response.json();
+                console.log(data);
+            } else {
+                const [id] = extras;
+                const response = await fetch(`${URL}/${id}`);
+                const data = await response.json();
+                console.log(data);
+            }
+        }
+
+        else if (metodo === 'POST' && recurso === 'products') {
+            const [titulo, precio, categoria] = extras;
+
+            const body = '{ "title": "' + titulo + '", "price": "' + precio + '", "category": "' + categoria + '" }';
+
+            const response = await fetch(URL, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: body
+            });
+
+            const data = await response.json();
+            console.log("Producto creado:");
+            console.log(data);
+        }
+
+        else if (metodo === 'DELETE' && recurso === 'products') {
+            const [id] = extras;
+
+            const response = await fetch(`${URL}/${id}`, {
+                method: 'DELETE'
+            });
+
+            const data = await response.json();
+            console.log("Producto eliminado:");
+            console.log(data);
+        }
+
+        else {
+            console.log("Comando no reconocido. Usá uno de estos:");
+            console.log("npm run start GET products");
+            console.log("npm run start GET products 15");
+            console.log("npm run start GET products/15");
+            console.log("npm run start POST products Remera 300 ropa");
+            console.log("npm run start DELETE products 7");
+        }
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+fetchData();
